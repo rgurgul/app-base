04e5d3f35a031249a74e75373f384a3940b7a2a5
rgurgul <gurgul.robert@gmail.com>
2021-06-06 09:32:20 +0200

    x

--- src/app/features/items/items.module.ts
+++ src/app/features/items/items.module.ts
@@ -1,12 +1,18 @@
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ItemsComponent } from './items/items.component';
+
 
 @NgModule({
   declarations: [
     ItemsComponent
   ],
   imports: [
-
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', component: ItemsComponent },
+    ])
   ]
 })
 export class ItemsModule { }
